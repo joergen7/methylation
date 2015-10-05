b76ae1055a4114840e4e24216e32b2f03a989a61
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-05 13:53:11 +0200

    Workflow improved.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,14 @@
 name 'methylation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer "Jörgen Brandt"
+maintainer_email "brandjoe@hu-berlin.de"
 license 'all_rights'
 description 'Installs/Configures methylation'
 long_description 'Installs/Configures methylation'
 version '0.1.0'
+
+recipe "methylation::_common", "Performs an update of the package system."
+recipe "methylation::default", "Sets up all preconditions to run the methylation workflow."
+
+
+depends "chef-bioinf-worker"
+depends "chef-cuneiform"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,18 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-include_recipe "chef-bioinf-worker::bismark"
-include_recipe "chef-bioinf-worker::cutadapt"
-include_recipe "chef-bioinf-worker::fastqc"
+#include_recipe "chef-bioinf-worker::cutadapt"
+#include_recipe "chef-bioinf-worker::fastqc"
+#include_recipe "chef-bioinf-worker::bsmooth"
+#include_recipe "chef-bioinf-worker::bsseq"
+#include_recipe "chef-bioinf-worker::sratools"
+#include_recipe "chef-bioinf-worker::shrimp"
+#include_recipe "chef-bioinf-worker::pass"
+#include_recipe "chef-bioinf-worker::bfast"
+include_recipe "chef-bioinf-worker::hg19"
 include_recipe "chef-bioinf-worker::sherman"
-include_recipe "chef-bioinf-worker::bsmooth"
-include_recipe "chef-bioinf-worker::bsseq"
-include_recipe "chef-bioinf-worker::sratools"
 include_recipe "chef-bioinf-worker::trimgalore"
-include_recipe "chef-bioinf-worker::hg38"
-include_recipe "chef-bioinf-worker::shrimp"
-include_recipe "chef-bioinf-worker::pass"
-include_recipe "chef-bioinf-worker::bfast"
+include_recipe "chef-bioinf-worker::bismark"
 include_recipe "chef-cuneiform::default"
 
 directory node.dir.wf
