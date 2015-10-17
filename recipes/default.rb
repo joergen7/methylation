0fb20b0ee1d6f56da52e04bb987fae6ffc984978
Jorgen Brandt <joergen.brandt@onlinehome.de>
2015-10-17 13:03:18 +0200

    Unnecessary includes deleted.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,14 +4,6 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-#include_recipe "chef-bioinf-worker::cutadapt"
-#include_recipe "chef-bioinf-worker::fastqc"
-#include_recipe "chef-bioinf-worker::bsmooth"
-#include_recipe "chef-bioinf-worker::bsseq"
-#include_recipe "chef-bioinf-worker::sratools"
-#include_recipe "chef-bioinf-worker::shrimp"
-#include_recipe "chef-bioinf-worker::pass"
-#include_recipe "chef-bioinf-worker::bfast"
 include_recipe "chef-bioinf-worker::hg19"
 include_recipe "chef-bioinf-worker::sherman"
 include_recipe "chef-bioinf-worker::trimgalore"
